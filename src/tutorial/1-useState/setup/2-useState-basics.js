80fda2e81a3fc4b92a117f71d8b8f0b6336f9382
Luis Sucuc <luissucuco@gmail.com>
2021-02-23 23:13:33 -0600

    useState

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("First");
+  const handleclick = () => {
+    if (text === "Ok") {
+      setText("Bye");
+    } else {
+      setText("Ok");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button type="button" className="btn" onClick={handleclick}>
+        Change
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
